641e740a7479d1d7e7fb354230531697e19f47ef
MarcPlanella <1493397@campus.euss.org>
2020-03-31 14:01:41 +0200

    Versió final

--- TCP_servidor/tcpservidor_main.c
+++ TCP_servidor/tcpservidor_main.c
@@ -1,3 +1,17 @@
+/*Programa del servidor del grupo F
+ * 
+ * Programa basado en el ejemplo publicado en EUSSternet por parte de los profesores de la asignatura 
+ * 
+ * 
+ * */
+
+
+
+
+
+
+
+
 
 
 #ifdef HAVE_CONFIG_H
@@ -65,7 +79,9 @@ int main(int argc, char *argv[])
 	result = listen(sFd, SERVER_MAX_CONNECTIONS);
 	
 	/*Bucle s'acceptació de connexions*/
+	printf("El servidor está conectado\n\n"); 
 	while(1){
+		printf("-----------------------------------------------"); 
 		printf("\nServidor esperant connexions\n");
 
 		/*Esperar conexió. sFd: socket pare, newFd: socket fill*/
@@ -78,111 +94,97 @@ int main(int argc, char *argv[])
 		printf("Missatge rebut del client(bytes %d): %s\n",	result, buffer);
 
 
-//iniciamos el tratamiento de la señal que nos ha pedido el cliente
-		char accion=buffer[1]; 
-		float medidas [20]; 
-		char marcha;
 		
-		char caracter1; 
-		char caracter2;
+		char accion=buffer[1];  //Variable de control de la estructura switch case, en función la letra recibida
+		float medidas [20]; 	//Array de medidas simuladas
+		char marcha;			//Variable para determinar si arrancar o apagar la toma de medidas
 		
+		//Definimos las variables para detectar los errores en la comunicación
+		char caracter1;         //Lee el primer {
+		char caracter2;			//Lee el último }
+		char caractertemps1;	//Lee el bit de las decenas del tiempo de muestreo
+		char caractertemps2;	//Lee el bit de las unidades del tiempo de muestreo
+		char caracternum;		//Lee el número de carácteres para hacer la media
 		
-		char caractertemps1;
-		char caractertemps2;
-		char caracternum;
-		int temps1;
-		int temps2;
-		int num;
-		//char caracter3 [1];
-		
-		switch (accion)
-			{
-			case 'M':      //Case principal de control
-			marcha=buffer[2];
+		switch (accion){	//Swich case que escoge como actuar en función de las peticiones del cliente
+			case 'm': 
+			case 'M':    
+			marcha=buffer[2];   			//Leemos la "v" del mensaje enviado por el cliente
 						
-			caracter1=buffer[0]; 
-			caracter2=buffer[6];
-			caractertemps1=buffer[3];
-			caractertemps2=buffer[4];
-			caracternum=buffer[5];
+			caracter1=buffer[0]; 			//Leemos el primer {
+			caracter2=buffer[6];			//Leemos el último }
+			caractertemps1=buffer[3]-48;		//Leemos las decenas del tiempo de muestreo y corregimos el valor asci
+			caractertemps2=buffer[4]-48;		//Leemos las unidades del tiempo de muestreo y corregimos el valor asci
+			caracternum=buffer[5]-48;			//Leemos el número de cifras para hacer la medias y corregimos el valor asci
+			 
 			
-			temps1= atoi(&caractertemps1);
-			temps2= atoi(&caractertemps2);
-			num= atoi(&caracternum);
-						
-			
-				if (caracter1=='{' && caracter2=='}') {
-					
-					if (marcha=='0' && temps1>=0 && temps1<=2 && temps2>=0 && temps2<=9 && num>=0 && num<=9){      //Apagamos el muestreo
-					printf("We are off boys\n"); 
-					    			
-						for (int i=0; i<20; i++) {      //Ponemos a 0 todas las muestras
-							medidas[i]=0; 
-						}
-					
-					strcpy(buffer2, "{M0}");        //Copiamos el mensaje en el buffer
+			 
+				if (caracter1=='{' && caracter2=='}') {		//Comprobamos que el inicio y final de trama es correcto
+					if (marcha=='0' && caractertemps1>=0 && caractertemps1<=2 && caractertemps2>=0 && caractertemps2<=9 && caracternum>=0 && caracternum<=9){      //Decidimos si la toma de muestras ha de detenerse a la vez que comprobamos que los parámetros están dentro de rango
+						printf("La toma de muestras ha sido detenida\n\n"); 	
+						strcpy(buffer2, "{M0}");      //Copiamos el mensaje en el buffer
 					}
 				
-					else if (marcha=='1' && temps1>=0 && temps1<=2 && temps2>=0 && temps2<=9 && num>=0 && num<=9){                               //Encendemos el muestro
-					printf("We are back bbys\n"); 
-						for (int i=0; i<20; i++) {     //Simulamos que tomamos muestras, ignoramos el tiempo
+					else if (marcha=='1' /*&& temps1>=0 && temps1<=2 && temps2>=0 && temps2<=9 && num>=0 && num<=9*/){      //Decidimos si la toma de muestras ha de iniciarse a la vez que comprobamos que los parámetros están dentro de rango
+						printf("La toma de muestras ah sido iniciada\n"); 
+						for (int i=0; i<20; i++) {     //Simulamos que tomamos muestras, ignoramos el tiempo de toma de muestras
 							medidas[i]=i; 
 						}
-					strcpy(buffer2, "{M0}");        //Copiamos el mensaje en el buffer
+						strcpy(buffer2, "{M0}");        //Copiamos el mensaje en el buffer
+						printf("El timepo de muestreo demandado es de:  %d%d ms\n", caractertemps1,caractertemps2); 
+						printf("El número de muestras demandado para hacer la media es de: %d medidas\n",caracternum); 
 					}
 					
 					else{
-					printf("El valor no es ni 0 ni 1\n");
-					strcpy(buffer2, "{M2}"); 
+						printf("El valor de la ""v"" no es ni 0 ni 1 \n");
+						strcpy(buffer2, "{M2}"); 
 					}
-					//Por el momento no tenemos en cuenta el tiempo de muestreo	
+					//Por el momento no tenemos en cuenta el tiempo de muestreo	ni el número de muestras para hacer la media
 				
 				}
-				else {
+				else {       //Si la trama es incorrecta entramos aqui
 				 strcpy(buffer2, "{M1}");
+				 printf("La trama recibida del cliente no es correcta"); 
 				}
-				break; 
-			
+			break; 
 		
-		
-			case 'x':						//Haremos el máximo
+			case 'x':					//Devolveremos el máximo del array
 			case 'X':
 			
-			caracter1=buffer[0]; 
-			caracter2=buffer[2]; 
+				caracter1=buffer[0]; 
+				caracter2=buffer[2]; 
 
-			printf("Nos han pedido el máximo\n");
-			
-								
-				for (int i=0; i<20; i++){
+				printf("Nos han pedido el máximo\n");						
+				for (int i=0; i<20; i++){		//Calculamos le máximo dentro del array
 					if (medidas[i]> mayor){
-							mayor=medidas[i];
+						mayor=medidas[i];
 					} 
 				}
-				
-				enterACadena(mayor,grande);
-					
-				
-				if (caracter1=='{' && caracter2=='}') {
-					strcpy(buffer2,"{X0"); 
-					}			
-				else{
-					printf("El valor no es ni 0 ni 1\n");
+				enterACadena(mayor,grande);		//Función que convierte un número float a letras
+				if (caracter1=='{' && caracter2=='}') {		//Escogemos si el código de error ha de ser '0' o '1'
+					if (mayor<10){							//Ajustamos el mensaje en función de si el número a enviar es superior o inferior a 10
+					strcpy(buffer2,"{X00");
+					}
+					else {
+					strcpy(buffer2,"{X0");
+					}
+				}			
+				else  {
+					printf("El fomrato del pedido no es correcto \n");
 					strcpy(buffer2, "{X1"); 
 					}
 								
-				strcat(buffer2,grande); //Copiar missatge a buffer
+				strcat(buffer2,grande);       //Acabamos de formar el mensaje
 				strcat(buffer2, "}");
-
+				printf("El valor de la medida máxima es; %.2f ºC\n", mayor); 
 			break; 
 			
-			case 'y':                     //Haremos el mínimo
+			case 'y':                     //Devolveremos el mínimo del array
 			case 'Y':
 			
 				caracter1=buffer[0]; 
 				caracter2=buffer[2]; 
-				
-				printf("Nos han pedido el menor\n"); 
+				printf("Nos han pedido el mínimo\n"); 
 				for (int i=0; i<20; i++){
 					if (medidas[i]<menor){
 						menor=medidas[i];
@@ -192,11 +194,11 @@ int main(int argc, char *argv[])
 				
 				if (caracter1=='{' && caracter2=='}') {
 				
-					if (menor<10){
-					strcpy(buffer2,"{Y00");
+					if (menor<10){				
+						strcpy(buffer2,"{Y00");
 					}
 					else {
-					strcpy(buffer2,"{Y0");
+						strcpy(buffer2,"{Y0");
 					}
 				}
 				
@@ -211,28 +213,28 @@ int main(int argc, char *argv[])
 						
 				strcat(buffer2,pequeno); //Copiar missatge a buffer
 				strcat(buffer2, "}"); 
+				printf("El valor de la medida mínima es: %.2f ºC\n",menor); 
 			break; 
 			
-			case 'u': 					//Haremos el último
+			case 'u': 					//Devolveremos el último número del array
 			case 'U':
 			
-			caracter1=buffer[0]; 
-			caracter2=buffer[2]; 
+				caracter1=buffer[0]; 
+				caracter2=buffer[2]; 
 			
-			printf("Nos han pedido el último\n"); 
-			//Concatenamos las diferentes partes
-			enterACadena(medidas[20],ultimo);
+				printf("Nos han pedido el último\n"); 
+				enterACadena(medidas[19],ultimo);		//Puesto que conocemos el tamaño del array de medidas simualda, nos limitamos a devolver el valor guardado en la última posicion
 			
 				if (caracter1=='{' && caracter2=='}') {
-				
 					strcpy(buffer2,"{U0"); 
 				}
+				
 				else{
 					strcpy(buffer2,"{U1");
 				}
-				
-			strcat(buffer2,ultimo); //Copiar missatge a buffer
-			strcat(buffer2, "}");
+				printf("El valor de la última medida del array es: %.2f\n", medidas[19]); 
+				strcat(buffer2,ultimo); 
+				strcat(buffer2, "}");
 
 
 			break; 
@@ -240,12 +242,12 @@ int main(int argc, char *argv[])
 			case 'r':						//Resetearemos los registros
 			case 'R':
 			
-			caracter1=buffer[0]; 
-			caracter2=buffer[2]; 
+				caracter1=buffer[0]; 
+				caracter2=buffer[2]; 
 			
-			printf("Nos han pedido cargarnos el máximo y mínimo\n"); 
-			mayor=0; 
-			menor=0; 
+				printf("Nos han pedido cargarnos el máximo y mínimo\n"); 
+				mayor=0; 
+				menor=0; 
 			
 				if (caracter1=='{' && caracter2=='}') {
 					strcpy(buffer2, "{R0}"); 
@@ -253,6 +255,7 @@ int main(int argc, char *argv[])
 				else {
 					strcpy(buffer2, "{R1}");
 				}
+				printf("Se han restablecido los valores máximo y mínimo\n"); 
 			
 			break; 
 			
@@ -260,38 +263,32 @@ int main(int argc, char *argv[])
 			case 'B':						//diremos el tamaño del array
 			case 'b':
 			
-			caracter1=buffer[0]; 
-			caracter2=buffer[2]; 
-			
-			
-			printf("Nos han pedido el número de muestras\n"); 
-			
+				caracter1=buffer[0]; 
+				caracter2=buffer[2];
+				printf("Nos han pedido el número de muestras\n"); 
 				if (caracter1=='{' && caracter2=='}') {
-					strcpy(buffer2,"{B020.0}"); 
+					strcpy(buffer2,"{B020.0}"); 		//Puestos que el array simualdo tiene un tamaño constante, podemos escribir el tamaño por texto de forma directa
 				}
 				else{
 					strcpy(buffer2,"{B120.0}"); 
 				}
+				printf("El tamaño del array de medidas es de 20 posiciones\n"); 
 			break;
 			
-			/*default:
-			caracter3[0]=accion;
-			printf("Cadena caracter3: %s \n", caracter3);
-			strcpy(buffer2,"{");
-			strcat(buffer2,caracter3);
-			strcat(buffer2,"2}");
-			*/
-			
-			}
-			
-			/*Enviar*/
-			result = write(newFd, buffer2, strlen(buffer2)+1); //+1 per enviar el 0 final de cadena
-			printf("Missatge enviat a client(bytes %d): %s\n",	result, buffer2);
-			
-			/*Tancar el socket fill*/
-			result = close(newFd);
-			
+			default:						//En caso de que la letra de accion no se entienda, saltará un error por parámetro fuera de rango
+			strcpy(buffer2,"{E2}");			//Dado que la letra recibida en este caso carecerá de sentido, enviamos por defecto la letra 'E', de error
+			printf("No se ha podido entender la letra de acción, devolvemos error 2\n"); 
+			break; 
 		}
+			
+		/*Enviar*/
+		result = write(newFd, buffer2, strlen(buffer2)+1); //+1 per enviar el 0 final de cadena
+		printf("Missatge enviat a client(bytes %d): %s\n",	result, buffer2);
+		/*Tancar el socket fill*/
+		result = close(newFd);
+		printf("Acción demandada por el cliente finalizada\n"); 
+		printf("-----------------------------------------------\n\n\n\n"); 
+	}
 }
 void enterACadena(float numero, char *bufer){
     sprintf(bufer, "%.2f", numero);
